04282d38e49908f88b7e1880b81454be781a39ee
1170801403 <1733108435@qq.com>
2019-07-07 14:26:35 +0800

    second commit

--- Hello.java
+++ Hello.java
@@ -2,5 +2,6 @@ public class Hello{
     public static void main(String[] args){
 	System.out.println("Hello world!");
 	System.out.println("Welcome to the open source world!");
+	System.out.println("Open Source Software development offers a unique opportunity for you to experience and participate in large scale software development")
     }
 }
